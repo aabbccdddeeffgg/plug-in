e9724b50358d038e4b728f59331e3103843739d7
aaccccdddeeffgg <wangyanqi602.gmail.com>
2024-09-25 20:24:51 +0800

    获取用户搜索框内容

--- article_content.js
+++ article_content.js
@@ -5,8 +5,8 @@ window.onload = function() {
   if (titleElement) {
     const articleTitle = titleElement.innerText.trim(); // 获取文章标题
     // 使用 chrome.runtime.getURL() 方法将图片转为可访问的 URL
-    const str = `image/${articleTitle}.jpg`;
-    const imgUrl = chrome.runtime.getURL(str);
+    const src = `image/${articleTitle}.jpg`;
+    const imgUrl = chrome.runtime.getURL(src);
 
     // 检查文件是否存在
     async function checkFileExists(url) {
@@ -21,7 +21,7 @@ window.onload = function() {
     checkFileExists(imgUrl)
       .then(exists => {
         if (exists) {
-          // 文件存在，创建一个 img 标签并插入到标题下方
+          // 文件存在， 创建一个 img 标签并插入到标题下方
           const imgElement = document.createElement('img');
           imgElement.src = imgUrl;
           imgElement.style.width = "100%"; // 设置图片宽度为100%
